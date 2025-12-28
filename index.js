a440873d8c69633c1608374b500cc2a540a51720
NIRANJAN-PATIL-1811 <rosh.rp1811@gmail.com>
2025-12-28 18:09:21 +0530

    updated code

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3');
+require('dotenv').config();
 
 const app = express();
 
@@ -12,6 +14,26 @@ app.get('/login', (req, res) => {
 
 app.get('/logout', (req, res) => {
   res.send('logout printed!');
+});
+
+app.get('/send', async (req, res) => {
+  const s3 = new S3Client(
+    {
+      region: process.env.AWS_REGION
+    }
+  );
+
+  await s3.send(
+    new PutObjectCommand(
+      {
+        Bucket: process.env.AWS_BUCKET,
+        Key: "File.txt",
+        Body: 'This is sample text from File.txt to S3!'
+      }
+    )
+  );
+
+  res.send('Sent! Please check in the bucket the file must be in!');
 });
 
 app.listen(3000, '0.0.0.0', () => {
